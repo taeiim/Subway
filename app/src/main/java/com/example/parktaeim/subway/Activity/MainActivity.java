58a8c91afd37c0858cd03a6816df3d916f92ea0e
taeiim <ti535666@gmail.com>
2018-04-17 23:59:11 +0900

    Fix tab color / Finish EventDetail xml

--- app/src/main/java/com/example/parktaeim/subway/Activity/MainActivity.java
+++ app/src/main/java/com/example/parktaeim/subway/Activity/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.parktaeim.subway.Activity;
 
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
 import com.example.parktaeim.subway.Fragment.EventFragment;
 import com.example.parktaeim.subway.Fragment.HoneyFragment;
 import com.example.parktaeim.subway.Fragment.MenuFragment;
@@ -21,52 +20,58 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.main_tabLayout);
-        tabLayout.addTab(tabLayout.newTab().setText("꿀조합").setIcon(R.drawable.icon_menu));
-        tabLayout.addTab(tabLayout.newTab().setText("메뉴"));
-        tabLayout.addTab(tabLayout.newTab().setText("매장찾기"));
-        tabLayout.addTab(tabLayout.newTab().setText("이벤트"));
-        tabLayout.addTab(tabLayout.newTab().setText("마이페이지"));
+        setTabLayout();
+    }
 
-//        for (int i = 0; i < tabLayout.getTabWidget().getChildCount(); i++)
-//        {
-//            tabLayout.getTabWidget().getChildAt(i).setPadding(10,10,10,10);
-//        }
+    private void setTabLayout() {
+        AHBottomNavigation bottomTabLayout = (AHBottomNavigation) findViewById(R.id.main_tabLayout);
 
-        setUpFragment(new HoneyFragment());
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+        AHBottomNavigationItem item1 = new AHBottomNavigationItem("꿀조합", R.drawable.ic_menu);
+        AHBottomNavigationItem item2 = new AHBottomNavigationItem("메뉴", R.drawable.ic_menu);
+        AHBottomNavigationItem item3 = new AHBottomNavigationItem("매장찾기", R.drawable.ic_store);
+        AHBottomNavigationItem item4 = new AHBottomNavigationItem("이벤트", R.drawable.ic_menu);
+        AHBottomNavigationItem item5 = new AHBottomNavigationItem("마이페이지", R.drawable.ic_menu);
+
+        bottomTabLayout.addItem(item1);
+        bottomTabLayout.addItem(item2);
+        bottomTabLayout.addItem(item3);
+        bottomTabLayout.addItem(item4);
+        bottomTabLayout.addItem(item5);
+
+        bottomTabLayout.setAccentColor(Color.parseColor("#029545"));
+        bottomTabLayout.setInactiveColor(Color.parseColor("#c4c4c4"));
+        bottomTabLayout.setTitleState(AHBottomNavigation.TitleState.ALWAYS_SHOW);
+        bottomTabLayout.setCurrentItem(0);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HoneyFragment()).commit();
+        bottomTabLayout.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
             @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                if(tab.getPosition() == 0){
-                    setUpFragment(new HoneyFragment());
-                } else if(tab.getPosition() == 1){
-                    setUpFragment(new MenuFragment());
-                } else if(tab.getPosition() == 2){
-                    setUpFragment(new StoreFragment());
-                } else if(tab.getPosition() == 3){
-                    setUpFragment(new EventFragment());
-                } else if(tab.getPosition() == 4){
-                    setUpFragment(new MypageFragment());
+            public boolean onTabSelected(int position, boolean wasSelected) {
+                switch (position) {
+                    case 0:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HoneyFragment()).commit();
+                        break;
+                    case 1:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MenuFragment()).commit();
+                        break;
+                    case 2:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new StoreFragment()).commit();
+                        break;
+                    case 3:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new EventFragment()).commit();
+                        break;
+                    case 4:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MypageFragment()).commit();
+                        break;
                 }
+                return true;
             }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
+        });
+        bottomTabLayout.setOnNavigationPositionListener(new AHBottomNavigation.OnNavigationPositionListener() {
+            @Override public void onPositionChange(int y) {
+                // Manage the new y position
             }
         });
     }
 
-    private void setUpFragment(Fragment fragment){
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction transaction =fragmentManager.beginTransaction();
-        transaction.replace(R.id.fragment_container,fragment);
-
-        transaction.commit();
-    }
 }
