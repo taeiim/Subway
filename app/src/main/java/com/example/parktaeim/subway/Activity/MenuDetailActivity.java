0609622ec3ebfa7c4ece22b01e54704eb53e9c57
taeiim <ti535666@gmail.com>
2018-04-23 00:59:16 +0900

    Finish MenuDetail xml

---
+++ app/src/main/java/com/example/parktaeim/subway/Activity/MenuDetailActivity.java
@@ -0,0 +1,21 @@
+package com.example.parktaeim.subway.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.parktaeim.subway.R;
+
+/**
+ * Created by parktaeim on 2018. 4. 22..
+ */
+
+public class MenuDetailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_detail);
+
+
+    }
+}
