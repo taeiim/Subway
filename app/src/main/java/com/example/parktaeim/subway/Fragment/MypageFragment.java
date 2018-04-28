14e25743c94983b233b9f623cd2dfbef76a08fd1
taeiim <ti535666@gmail.com>
2018-04-28 21:47:43 +0900

    Add CollapsingToolbar in mypage

--- app/src/main/java/com/example/parktaeim/subway/Fragment/MypageFragment.java
+++ app/src/main/java/com/example/parktaeim/subway/Fragment/MypageFragment.java
@@ -1,10 +1,27 @@
 package com.example.parktaeim.subway.Fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.parktaeim.subway.R;
 
 /**
  * Created by parktaeim on 2018. 4. 15..
  */
 
 public class MypageFragment extends Fragment {
+    private View rootView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_mypage,container,false);
+        return rootView;
+    }
+
 }
