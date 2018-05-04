7a1efe5ae56ce0a2b7ac35c354f9ae6c87efe6a7
taeiim <ti535666@gmail.com>
2018-05-04 20:34:54 +0900

    Change to vertical tablayout in EnrollActivity

--- app/src/main/java/com/example/parktaeim/subway/Activity/HoneyEnrollmentActivity.java
+++ app/src/main/java/com/example/parktaeim/subway/Activity/HoneyEnrollmentActivity.java
@@ -6,6 +6,10 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
 import android.widget.RelativeLayout;
 
 import com.example.parktaeim.subway.Adapter.VerticalViewpagerAdapter;
@@ -22,6 +26,7 @@ public class HoneyEnrollmentActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_honey_enrollment);
 
+
         RelativeLayout cancelIcon = (RelativeLayout) findViewById(R.id.honeyEnroll_cancelBtn);
         cancelIcon.setOnClickListener(v -> finish());
 
@@ -33,6 +38,10 @@ public class HoneyEnrollmentActivity extends FragmentActivity {
         verticalViewPager.setAdapter(new VerticalViewpagerAdapter(getSupportFragmentManager()));
         TabLayout tabLayout = (TabLayout) findViewById(R.id.honeyEnroll_tabLayout);
         tabLayout.setupWithViewPager(verticalViewPager,true);
+//        tabLayout.setRotation(tabLayout.getRotation()+90);
 
+        tabLayout.setAnimation(AnimationUtils.loadAnimation(this,R.anim.rotate_tablayout));
+//        tabLayout.setEnabled(false);
+//        tabLayout.setClickable(false);
     }
 }
